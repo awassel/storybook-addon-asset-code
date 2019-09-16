7a7fd6db0144e48f9f4f594fa889d7debbd0e48e
Addam Wassel <AWassel@rightpoint.com>
2019-09-16 10:54:55 -0400

    Updates for button styles

--- packages/storybook-react/stories/button/button.jsx
+++ packages/storybook-react/stories/button/button.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { styled } from '@storybook/theming';
+import './button.css';
 
 const StyledButton = styled.button({
     height: '100%',
     overflow: 'auto',
-    width: '100%',
 });
 
 const button = () => {
-    return (<StyledButton>Hello Button</StyledButton>)
+    return (<StyledButton className="button">Hello Button</StyledButton>)
 };
 
 export default button;
